706a739a5fefb0c41f14d9fd9cc3a4d3a6614294
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2025-06-30 13:12:22 -0300

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/app/components/Header";
 import { ContactForm } from "./contact/components";
 import { Toaster } from "./components/toaster";
-import SportsBanner from "./components/Banner";
 import TeamSection from "./about/components/team-section";
 import CategoriesShowcase from "./components/categories-showcase";
 
